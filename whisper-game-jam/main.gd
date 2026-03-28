fe6ae7a3831b30069f7cea01c5baad857837cca0
Zoe Bisset <lemonadecode@gmail.com>
2026-03-28 17:26:52 -0400

    Updated My godot

--- whisper-game-jam/main.gd
+++ whisper-game-jam/main.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file()
+	get_tree().change_scene_to_file("res://scenes/loading.tscn")
 
 
 func _on_settings_pressed() -> void:
